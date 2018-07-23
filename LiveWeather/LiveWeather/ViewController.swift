05557be3a21243eb8244938ce3c1410eddbabfb5
Do Hoang Viet <vietdh2@fsoft.com.vn>
2018-07-23 09:55:12 +0700

    Create project

---
+++ LiveWeather/LiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveWeather
+//
+//  Created by Do Hoang Viet on 7/23/18.
+//  Copyright © 2018 Do Hoang Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
