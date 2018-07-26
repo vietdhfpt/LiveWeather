739ed1ee883fabdbf650a5641359696ed422f0fb
Do Hoang Viet <vietdh2@fsoft.com.vn>
2018-07-26 09:36:03 +0700

    ParseJSON and get data LiveWeather

--- LiveWeather/LiveWeather/Manager/Manager.swift
+++ LiveWeather/LiveWeather/Manager/Manager.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+/// Comletion Handler Type
+typealias CompletionHandler = (Any?, Error?) -> Void
+
+class Manager {
+    
+    static let shared = Manager()
+    
+    func parseJSON(lat: Double, lon: Double, completionHandler: @escaping CompletionHandler) {
+        let weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=\(lat)&lon=\(lon)&cnt=10&mode=json&appid=c80a2e47667cedab4873abb8a9fff762"
+        
+        guard let url = URL(string: weatherUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            do {
+                guard let jsonData = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else { return }
+                let liveWeather = LiveWeather(json: jsonData)
+                completionHandler(liveWeather, nil)
+            } catch let error {
+                completionHandler(nil, error)
+            }
+        }.resume()
+    }
+    
+}
--- LiveWeather/LiveWeather/ViewController.swift
+++ LiveWeather/LiveWeather/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let lat: Double = 21.013493
+    let lon: Double = 105.796525
+    
+    var liveWeather: LiveWeather?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        Manager.shared.parseJSON(lat: lat, lon: lon) { (data, error) in
+            guard let liveWeather = data as? LiveWeather else { return }
+            self.liveWeather = liveWeather
+        }
     }
-
-
 }
 
